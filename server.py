f00ae7712cdf7f7d2fbb3e5b1653bd9877ce95ab
Animesh <annimesh2809@gmail.com>
2016-10-29 12:54:05 +0530

    Changes made

--- server.py
+++ server.py
@@ -49,18 +49,48 @@ while True:
 				client.close()
 		else:
 			msg = pickle.loads(r.recv(RECV_BUFFER))
-			name,msg=msg.split('@')
+			try:
+				name,msg=msg.split('@')
+			except:
+				print("Bad command from",getNameFromClient(r,clients))
+				r.send(pickle.dumps("Bad command\nSyntax:\n\t[Username] attherate [Message]\n\tUsername can be blank for server commands\n"))
+				continue
 			name=name.strip()
 			msg=msg.strip()
-			if NameIsNotPresent(name,clients):
+			if name == "":				#For server commands
+				commands = msg.split(" ")
+				for command in commands:
+					command.strip()
+				typeofcomm = len(commands)
+				if typeofcomm == 1:
+					if commands[0] == "allonline":
+						data = ""
+						for client in clients:
+							data=data+"\n\t"+client["name"]
+						r.send(pickle.dumps(data))
+						print(getNameFromClient(r,clients),"requested for service: allonline")
+				if typeofcomm == 2:
+					if commands[0] == "isonline":
+						if NameIsNotPresent(commands[1],clients):
+							r.send(pickle.dumps(commands[1]+" is not online\n"))
+						else:
+							r.send(pickle.dumps(commands[1]+" is online\n"))
+
+			elif NameIsNotPresent(name,clients):
 				r.send(pickle.dumps("User with username: "+name+" is offline"))
 				continue
+
 			else:
 				for client in clients:
 					if client["name"] == name:
 						sender = getNameFromClient(r,clients)
 						msg=msg+"@"+sender
-						client["client"].send(pickle.dumps(msg))
-						msg=msg.split('@')[0]
-						print("Sent to",name,"message: \"",msg,"\" from",sender)
-						break
+						try:
+							client["client"].send(pickle.dumps(msg))
+							msg=msg.split('@')[0]
+							print("Sent to",name,"message: \"",msg,"\" from",sender)
+							break
+						except:
+							r.send(pickle.dumps("User with username: "+name+" is offline"))
+							clients.remove(client)
+							client["client"].close()
